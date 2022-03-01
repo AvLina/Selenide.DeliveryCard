6d38b036947d8164e5414f3dac9a760d36fae5f2
AvLina <avlina3@gmail.com>
2022-03-01 10:20:33 +0300

    Adding code

---
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -0,0 +1,36 @@
+package ru.netology;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class DeliveryCardTest {
+
+    @Test
+    public void DeliveryCard() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Воронеж");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        String verificationDate = LocalDate.now().plusDays(4)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=date] input").setValue(verificationDate);
+        $("[data-test-id=name] input").setValue("Иван Иванов");
+        $("[data-test-id=phone] input").setValue("+79087654321");
+        $("[data-test-id=agreement]").click();
+        $(".button").shouldHave(text("Забронировать")).click();
+        $("[data-test-id=notification]")
+                .shouldHave(text("Успешно! Встреча успешно забронирована на " + verificationDate),
+                        Duration.ofSeconds(15));
+    }
+
+}
